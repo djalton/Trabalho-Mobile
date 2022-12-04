3f0a9b4a0936ba81a3edc71db1610b2b1136bf8e
Lenovo <pedrodjalton@gmail.com>
2022-12-04 18:50:51 -0300

    trabalho

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Splash from './src/pages/Splash/index';
+import Login from './src/pages/Login/index';
+import Cadastro from './src/pages/Cadastro/index';
+import Home from './src/pages/Home/index';
+import Agendamento from './src/pages/Agendamento/index';
+
+
+
+const Stack = createStackNavigator();
+
+export default function App(){
+  return(
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName='Splash'>
+      <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}}/>
+      <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+      <Stack.Screen name="Cadastro" component={Cadastro}/>
+      <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+      <Stack.Screen name="Agendamento" component={Agendamento}/>
+    </Stack.Navigator>
+    </NavigationContainer>
+);
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Agendamento-Hospitalar
+Agendamento hospitalar em Reac-Native
+
+
+![design](https://user-images.githubusercontent.com/79226722/200311080-9220ed28-ded1-487c-94bc-830b49b75122.png)
